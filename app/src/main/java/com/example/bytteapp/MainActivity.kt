a9a6a06cff85db0bc0378d6cc62acb178734cc1f
Elliot Lyhne <Hunterlyhne@gmail.com>
2026-03-05 13:02:03 +0100

    Wireframe og basic SVG's

--- app/src/main/java/com/example/bytteapp/MainActivity.kt
+++ app/src/main/java/com/example/bytteapp/MainActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bytteapp.ui.theme.BytteAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +37,159 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BytteAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            AppFrontPage()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppFrontPage(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "Recommended Articles",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(top = 50.dp)
+            )
+
+            LazyVerticalGrid(
+                columns = GridCells.FixedSize(190.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                items(10) {
+                    Article()
+                }
+            }
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(75.dp)
+                .background(color = Color.LightGray)
+                .align(alignment = Alignment.BottomStart)
+        ) {
+            val navIconSpacing = 10.dp
+
+            Image(
+                painterResource(R.drawable.stud),
+                contentDescription = "Home",
+                modifier = Modifier.padding(navIconSpacing)
+            )
+            Image(
+                painterResource(R.drawable.stud),
+                contentDescription = "Home",
+                modifier = Modifier.padding(navIconSpacing)
+            )
+            Image(
+                painterResource(R.drawable.stud),
+                contentDescription = "Home",
+                modifier = Modifier.padding(navIconSpacing)
+            )
+            Image(
+                painterResource(R.drawable.stud),
+                contentDescription = "Home",
+                modifier = Modifier.padding(navIconSpacing)
+            )
+            Image(
+                painterResource(R.drawable.stud),
+                contentDescription = "Home",
+                modifier = Modifier.padding(navIconSpacing)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BytteAppTheme {
-        Greeting("Android")
+fun Article() {
+
+    Box(
+        modifier = Modifier
+            .width(190.dp)
+            .height(400.dp)
+            .padding(5.dp)
+    ) {
+        // Content eks. heart
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            // Image card
+            Image(
+                painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = "Lol",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .background(
+                        color = Color.LightGray,
+                        shape = RoundedCornerShape(20.dp)
+                    )
+            )
+
+            // Title, description and price container
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 15.dp)
+            ) {
+                // Title
+                Text(
+                    text = "Lorem Ipsum",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(top = 5.dp)
+                )
+
+                // Description
+                Text(
+                    text = "Quisque ut dolor gravida, placerat libero vel, euismod.",
+                    fontSize = 10.sp,
+                    modifier = Modifier
+                        .padding(top = 5.dp)
+                )
+
+                // Price
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(top = 5.dp)
+                ) {
+                    Image(
+                        painterResource(R.drawable.stud),
+                        contentDescription = "Placeholder",
+                        modifier = Modifier
+                            .width(35.dp)
+                            .padding(end = 10.dp)
+                    )
+                    Text(
+                        text = "420",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+            }
+        }
+
+        // heart
+        Image(
+            painterResource(R.drawable.heart_filled), // ikke fyldt hjerte: "heart"
+            contentDescription = "Heart icon",
+            modifier = Modifier
+                .width(60.dp)
+                .height(60.dp)
+                .align(alignment = Alignment.TopEnd)
+                .padding(10.dp)
+        )
     }
 }
